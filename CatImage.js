ed0a61aba595e03e34df3f9bc8641a12bbfc8310
Catherine Sauer <cathyrae903@gmail.com>
2020-11-14 09:13:28 -0600

    Add function for dynamic opacity updating

--- CatImage.js
+++ CatImage.js
@@ -1,100 +1,43 @@
 import { Dimensions, StyleSheet, ImageBackground } from 'react-native';
 import React from 'react';
-import { Content } from 'native-base';
+import { Content, Button, Text, View } from 'native-base';
 import { Surface } from 'gl-react-expo';
-import ImageFilters from 'react-native-gl-image-filters';
-import Filter from './Filter';
 import Tomb from './assets/tombstone.jpg'
+import FilteredImage from './FilteredImage';
 
 const width = Dimensions.get('window').width;
-
-// Min and Max range for each filter.
-const settings = [
-  {
-    name: 'hue',
-    minValue: 0,
-    maxValue: 6.3,
-  },
-  {
-    name: 'blur',
-    minValue: 0,
-    maxValue: 30,
-  },
-  {
-    name: 'sepia',
-    minValue: -5,
-    maxValue: 5,
-  },
-  {
-    name: 'sharpen',
-    minValue: 0,
-    maxValue: 15,
-  },
-  {
-    name: 'negative',
-    minValue: -2.0,
-    maxValue: 2.0,
-  },
-  {
-    name: 'contrast',
-    minValue: -10.0,
-    maxValue: 10.0,
-  },
-  {
-    name: 'saturation',
-    minValue: 0.0,
-    maxValue: 2,
-  },
-  {
-    name: 'brightness',
-    minValue: 0,
-    maxValue: 5,
-  },
-  {
-    name: 'temperature',
-    minValue: 0.0,
-    maxValue: 40000.0,
-  },
-];
 
 class CatImage extends React.Component {
 
   constructor (props) {
     super(props)
-
     this.state = {
-      ...settings,
-      hue: 0,
-      blur: 0,
-      sepia: 0,
-      sharpen: 0,
-      negative: 0,
-      contrast: 1,
-      saturation: 1,
-      brightness: 1,
-      temperature: 6500,
+      catOpacity: 1
     };
   }
+
+  updateOpacity = async () => {
+    this.setState({ catOpacity: 0 })
+    // console.warn('new opacity: ', this.state.catOpacity);
+  };
 
   render() {
     return (
         <Content style={styles.content} showsVerticalScrollIndicator={false}>
           <ImageBackground source={ Tomb } style={styles.tombstone}>
-            <Surface style={styles.modifiedCat} ref={ref => (this.image = ref)}>
-              <ImageFilters {...this.state} width={width} height={width}>
-                {{ uri: 'https://thiscatdoesnotexist.com' }}
-              </ImageFilters>
-            </Surface>
+            <View style={{opacity: this.state.catOpacity}}>
+              <Surface style={styles.modifiedCat} ref={ref => (this.image = ref)}>
+                <FilteredImage/>
+              </Surface>
+            </View>
           </ImageBackground>
-          {settings.map(filter => (
-            <Filter
-              key={filter.name}
-              name={filter.name}
-              minimum={filter.minValue}
-              maximum={filter.maxValue}
-              onChange={value => this.setState({ [filter.name]: value })}
-            />
-          ))}
+          <Button
+            rounded={false}
+            style={styles.button}
+            block
+            onPress={this.updateOpacity}>
+            <Text>Change Opacity</Text>
+          </Button>
         </Content>
     );
   }
@@ -115,7 +58,6 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     width: width,
     height: width,
-    opacity: 0.8  // Change opacity for cat image
   }
 });
 
---
+++ FilteredImage.js
@@ -0,0 +1,84 @@
+import { Dimensions } from 'react-native';
+import React from 'react';
+import ImageFilters from 'react-native-gl-image-filters';
+
+const width = Dimensions.get('window').width;
+
+// Min and Max range for each filter.
+const settings = [
+  {
+    name: 'hue',
+    minValue: 0,
+    maxValue: 6.3,
+  },
+  {
+    name: 'blur',
+    minValue: 0,
+    maxValue: 30,
+  },
+  {
+    name: 'sepia',
+    minValue: -5,
+    maxValue: 5,
+  },
+  {
+    name: 'sharpen',
+    minValue: 0,
+    maxValue: 15,
+  },
+  {
+    name: 'negative',
+    minValue: -2.0,
+    maxValue: 2.0,
+  },
+  {
+    name: 'contrast',
+    minValue: -10.0,
+    maxValue: 10.0,
+  },
+  {
+    name: 'saturation',
+    minValue: 0.0,
+    maxValue: 2,
+  },
+  {
+    name: 'brightness',
+    minValue: 0,
+    maxValue: 5,
+  },
+  {
+    name: 'temperature',
+    minValue: 0.0,
+    maxValue: 40000.0,
+  },
+];
+
+class FilteredImage extends React.Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      ...settings,
+      hue: 0,
+      blur: 0,
+      sepia: 0,
+      sharpen: 0,
+      negative: 0,
+      contrast: 1,
+      saturation: 1,
+      brightness: 1,
+      temperature: 6500,
+    };
+  }
+
+  render() {
+    return (
+        <ImageFilters {...this.state} width={width} height={width}>
+        {{ uri: 'https://thiscatdoesnotexist.com' }}
+        </ImageFilters>
+    );
+  }
+}
+
+export default FilteredImage;
