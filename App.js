ed9fb0b5534374ecd4237cc2198ecbd4e4fa0371
Tanner May <tamfrb@umsystem.edu>
2020-11-14 13:13:27 -0600

    picture frames

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, Dimensions } from 'react-native';
 import CatImage from './CatImage';
+import Frame from './display';
+import topFrame from './assets/frame-tp.png';
+import bottomFrame from './assets/frameInverted-tp.png';
+
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <CatImage/>
-      <Text style={{color: '#888', fontSize: 30}}>
-          Hello World!
-        </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	
+  return ( <Frame/> );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
