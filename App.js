a6b5e4c7b078499df6b510994572d37b7f750cc2
Gavin <grncheetah10@gmail.com>
2020-11-13 23:14:29 -0600

    Can get pedometer data

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import logo from './assets/logo.png'; 
+import logo from './assets/logo.png';
+import StepData from './StepData';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{uri: 'https://thiscatdoesnotexist.com/'}} style={{ width: 305, height: 159 }} /> 
-      <Text style={{color: '#888', fontSize: 30}}>
-          Hello World!
-        </Text>
+      <Image source={{ uri: 'https://thiscatdoesnotexist.com/' }} style={{ width: 305, height: 159 }} />
+      <Text style={{ color: '#888', fontSize: 30 }}>
+        Hello World!
+      </Text>
+      <StepData></StepData>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,3 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
